fd617fee5615d9ce99e785d95db7e6a8c3f54a1a
maxxAbow <mbowman@welland.com>
2023-01-03 18:47:44 -0500

    initial files

---
+++ Develop/index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+// https://www.npmjs.com/package/inquirer/v/8.2.4
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is your title?',
+    'How would you describe the project',
+    'What are the installation instructions?',
+    'How is it used?',
+    'What type of Licensing does this project fall under?',
+    'Can anyone contribute to the project?',
+    'How can the project be tested?',
+    'What is your github username?',
+    'What is the email associated with your github?'
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
